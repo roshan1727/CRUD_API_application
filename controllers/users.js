7e61a4953c607a29b508c1e5910b9a9ff9580b3d
roshan1727 <rakeshroshanp27@gmail.com>
2024-02-27 15:25:39 +0530

    update

---
+++ controllers/users.js
@@ -0,0 +1,60 @@
+import {
+    v4 as uuidv4
+} from 'uuid';
+let users = [];
+export const createUser = (req, res) => {
+    console.log("POST route has done successfully");
+    const user = req.body;
+    const userId = uuidv4();
+    const userWithId = {
+        ...user,
+        id: userId
+    };
+    users.push(userWithId);
+
+    res.send(`POST route has done successfully and the user is ${user.Fname}`);
+};
+
+export const getUser = (req, res) => {
+    const {
+        id
+    } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+};
+
+export const deleteData = (req, res) => {
+    const {
+        id
+    } = req.params;
+    console.log(id);
+    users = users.filter((user) => user.id !== id);
+    res.send(`The deleted id: ${id} has been done successfully`);
+}
+
+export const editData = (req, res) => {
+    const uniqueId = req.params.id;
+    const {
+        fName,
+        Lname,
+        age
+    } = req.body; // Extract from req.body
+    let userDetail = users.find((user) => user.id === uniqueId);
+    console.log(userDetail.Fname, "/", userDetail.Lname);
+    if (fName) {
+        userDetail.Fname = fName;
+    }
+    if (Lname) {
+        userDetail.Lname = Lname;
+    }
+    if (age) {
+        userDetail.age = age;
+    }
+    res.send(`User with the id ${uniqueId} has been updated`);
+};
+
+
+export const getUsers = (req, res) => {
+    console.log(users);
+    res.send(users);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,26 @@
 import express from "express";
 import {
-    v4 as uuidv4
-} from 'uuid';
-
+    editData,
+    deleteData,
+    getUser,
+    createUser,
+    getUsers
+} from '../controllers/users.js'
 
 const router = express.Router();
-let users = []
-
-router.get('/', (req, res) => {
-    console.log(users);
-    res.send(users);
-})
-
-router.post('/', (req, res) => {
-    console.log("POST route has done sucessfully");
-    const user = req.body;
-    const userId = uuidv4();
-    const userWithId = {
-        ...user,
-        id: userId
-    }
-    users.push(userWithId);
-
-    res.send(`POST route has done sucessfully and the user is ${user.Fname}`);
-});
 
 
-// get the specific user deatil usoing the unique id
-router.get('/:id', (req, res) => {
-    const {
-        id
-    } = req.params;
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-});
+router.get('/', getUsers);
 
-// now deleteing the user based on the id
-router.delete("/:id", (req, res) => {
-    const {
-        userid
-    } = req.params;
-    console.log(userid);
-    users = users.filter((user) => user.id !== userid);
-    res.send(`The deleted id:${userid} has been done successfully`);
-});
+router.post('/', createUser);
 
+// get the specific user detail using the unique id
+router.get('/:id', getUser);
 
+// now deleting the user based on the id
+router.delete("/:id", deleteData);
 
-
+// to do a partial edit of the user detail using patch
+router.patch('/:id', editData);
 
 export default router;
